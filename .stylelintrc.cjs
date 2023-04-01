0dae2b08be34708885c0e3f45418e5d00b915bd7
Marc Görtz <mail@marcgoertz.de>
2023-04-01 17:42:57 +0200

    Move Stylelint configuration into its own file.

---
+++ .stylelintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'at-rule-no-vendor-prefix': true,
+    'color-named': 'never',
+    'font-family-name-quotes': 'always-where-recommended',
+    'function-url-quotes': [
+      'always',
+      {
+        except: ['empty'],
+      },
+    ],
+    'media-feature-name-no-vendor-prefix': true,
+    'no-descending-specificity': null,
+    'selector-attribute-quotes': 'always',
+    'selector-no-vendor-prefix': true,
+  },
+};
